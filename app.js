95276869b820d62f3baf671c58ae2aa24a1d87bd
muhammadwasil4455 <wasiltanoli37@gmail.com>
2025-11-03 23:37:49 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,104 @@
+let userName = document.getElementById("userName");
+let userPassword = document.getElementById("userPassword");
+
+
+
+let adminData = {
+    name: "wasil",
+    Password: 12345,
+}
+
+let { name, Password } = adminData
+
+function loginBtn() {
+
+    if (name == userName.value && Password == userPassword.value) {
+        location = "dashboard.html"
+    } else {
+        alert("invalid")
+    }
+
+}
+
+
+
+let arr = JSON.parse(localStorage.getItem("students")) || [];
+
+
+
+function addStudent() {
+    let stuName = document.getElementById("stuName")
+    let stuRollNum = document.getElementById("stuRollNum")
+    let stuClass = document.getElementById("stuClass");
+    let sudentDate = {
+        name: stuName.value,
+        roll: stuRollNum.value,
+        class: stuClass.value,
+    }
+
+    arr.push(sudentDate)
+    localStorage.setItem("students", JSON.stringify(arr));
+
+    stuName.value = "";
+    stuRollNum.value = "";
+    stuClass.value = "";
+
+}
+
+
+let attendencelist = document.getElementById("attendencelist");
+
+arr.forEach(function (student, index) {
+    attendencelist.innerHTML += `<div class = "list" >
+    <p>
+    ${index + 1} -
+    Student Name : (${student.name}
+    Roll number : (${student.roll})
+    class : (${student.class}) 
+    </p>
+    </div>`
+
+});
+
+function attendence() {
+    location = "attendence.html"
+}
+
+
+
+
+
+
+function markAttendence() {
+    let recordmainList = document.getElementById("recordmainList");
+    let input = document.getElementById("input");
+    let arr = JSON.parse(localStorage.getItem("students")) || [];
+
+    let found = arr.find(function (student) {
+        return student.roll === input.value
+    })
+    input.value = ""
+
+    if (found) {
+        found.status = "present";
+        localStorage.setItem("students", JSON.stringify(arr));
+        recordmainList.innerHTML += `<p>
+        student Name : (${found.name})
+        student Class : (${found.class})
+        student Roll number : (${found.roll})
+        status : (${found.status}).
+        </p>`
+    }
+    else {
+        alert("attendence not marked")
+    }
+
+}
+
+
+
+
+
+function recordList() {
+    location = "recordlist.html"
+}
